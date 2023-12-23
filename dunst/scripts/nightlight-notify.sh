aacbfb6a9a6bf4cf0a307bc24cbcea19c5f05e4a
dpi0 <dpi0.dev@proton.me>
2023-12-23 22:02:33 +0530

    added notify scripts

--- dunst/scripts/nightlight-notify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-msgID="4122"
-nightlight_info=$(busctl --user introspect rs.wl-gammarelay / rs.wl.gammarelay)
-nightlight=$(echo "$nightlight_info" | choose 3 | awk 'NR==5')
-color=#FF6D2EFF
-
-
-dunstify " $nightlight" -t 800 -r "$msgID" -h string:fgcolor:"$color" -h string:frcolor:"$color"
