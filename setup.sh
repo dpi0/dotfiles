3e407b75917d1123372a371fdb2f5649bfba9d19
dpi0 <dpi0.dev@proton.me>
2023-12-25 13:38:11 +0530

    minor tweaks

--- setup.sh
+++ setup.sh
@@ -30,10 +30,15 @@ for file in "${config_files[@]}"; do
     link_config "$file"
 done
 
-rm -f $CONFIG/starship.toml; ln -s $DOTFILES/starship/starship.toml $CONFIG/starship.toml
+rm -f $CONFIG/starship.toml; ln -s $DOTFILES/starship/starship.toml $CONFIG/
 rm -f $HOME/.zshrc; ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
 rm -f $HOME/.gitconfig; ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
 rm -f $HOME/.gitattributes; ln -s $DOTFILES/git/.gitattributes $HOME/.gitattributes
+
+md $HOME/.local/bin
+ln -s /data/scripts/bash/timer.sh $HOME/.local/bin/timer
+ln -s /data/scripts/bash/doo.sh $HOME/.local/bin/doo
+# ln -s /data/scripts/bash/dunst/* $HOME/.local/bin/
 
 # rm -f $CONFIG/libinput-gestures.conf; ln -s $DOTFILES/libinput-gestures/libinput-gestures.conf $CONFIG/libinput-gestures.conf
 # greetd config.toml
